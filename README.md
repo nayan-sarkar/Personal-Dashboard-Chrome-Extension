1e383d4acb4704dbe3a1094da2e734e54f7d0869
Nayan Sarkar <10292012+nayan-sarkar@users.noreply.github.com>
2023-09-30 15:42:12 -0700

    Upload Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Personal Dashboard Chrome Extension
+### Personal Dashboard Chrome Extension
+![Image](./app-image.png)
+- Built using ES6 `async-await` and `fetch` api
+- Fetched data from `unsplash api` for background images
+- Uses browsers inbuilt `navigator.geolocation.getCurrentPosition` method to get location data, which is the queries the open weather api to get weather Data
+- Uses Opengecko API to fetch crypto currency data
+
+### Built With 
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+/* Fetch Background Image */
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+async function fetchBackgroundImage(){
+    try{
+        const apiUrl = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=dark+abstract");
+        const data = await apiUrl.json();
+        document.body.style.backgroundImage = `url(${data.urls.regular})`;
+        document.getElementById("author").textContent = `By: ${data.user.name}`;
+    }
+    /* Use default background if no background found */
+    catch(error){
+        document.body.style.background = 'darkgreen';
+    }
+}
+
+fetchBackgroundImage();
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
